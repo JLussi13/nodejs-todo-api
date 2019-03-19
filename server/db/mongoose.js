a5f072113ba9ae9bac0e19f201fcd4b70e4f5103
James Lussier <ragnar0kx@live.com>
2019-03-19 03:43:14 -0500

    Logging whatever this error is

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,9 @@
 var mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
-console.log(process.env.MONGOLAB_URI)
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }).then(() => {
     console.log("Connected to database");
 }, (e) => {
-    console.log("Failed to connect to MongoDB");
+    console.log(e);
 });
 module.exports = {mongoose};
