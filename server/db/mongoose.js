39f2e36cee902377d34c895a4765fa21c0648a34
James Lussier <ragnar0kx@live.com>
2019-03-19 03:24:32 -0500

    Fulfilled missing promise

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,9 @@ var mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
 console.log(process.env.MONGOLAB_URI)
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }).then(() => {
+    console.log("Connected to database");
+}, (e) => {
+    console.log("Failed to connect to MongoDB");
+});
 module.exports = {mongoose};
